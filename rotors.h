f314d715d4f841d0d35f6071c0158a6e439e8a66
Michael Lan <michael.lan20@imperial.ac.uk>
2020-11-13 13:30:36 +0000

    Added Rotor class

---
+++ rotors.h
@@ -0,0 +1,20 @@
+#ifndef ROTORS_H
+#define ROTORS_H
+
+#include <vector>
+using namespace std;
+
+class Rotor {
+    private:
+        vector<int> rotor_start_pos;
+        vector<int> mappings;
+        int rotor_ticks = 0;
+
+    public:
+        Rotor(vector<int> mappings, vector<int> rotor_start_pos);
+        int new_rotor(string rotors_config_file, Rotor** rotor);
+        int map_forwards(int letter, int& rotor_ticks);
+        // int map_backwards(/*...*/); need to implement
+};
+
+#endif
