b15f6c8339105546f9072ec35ce20a8e00cd7990
Michael Lan <michael.lan20@imperial.ac.uk>
2020-11-20 14:15:21 +0000

    Implemented encryption functions

--- enigma.cpp
+++ enigma.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 #include "plugboard.h"
 #include "reflector.h"
 #include "rotors.h"
 #include "errors.h"
 #include "enigma.h"
+#include "constants.h"
 
 using namespace std;
 
 Enigma::Enigma(Plugboard* plugboard, Reflector* reflector, vector<Rotor*> created_rotors, int number_of_rotors)
     : plugboard(plugboard), reflector(reflector), created_rotors(created_rotors), number_of_rotors(number_of_rotors) {}
 
+Enigma::~Enigma() {
+    delete plugboard;
+    delete reflector;
+    for (int i = 0; i < created_rotors.size(); i++) {
+        delete created_rotors[i]->prev;
+        delete created_rotors[i];
+    }
+}
+
 int Enigma::create_enigma_machine(int argc, char** argv, Enigma** enigma) {
-    int error_code, num_of_rotors;
+    int error_code, num_of_rotors = 0;
     string rotor_pos_config_file, plugboard_config_file, reflector_config_file;
     vector<string> rotor_files;
     vector<int> rotor_starting_positions;
@@ -29,14 +40,14 @@ int Enigma::create_enigma_machine(int argc, char** argv, Enigma** enigma) {
     reflector_config_file = argv[2];
 
     /* create plugboard */
-    Plugboard* plugboard;
+    Plugboard* plugboard = nullptr;
     error_code = plugboard->new_plugboard(plugboard_config_file, &plugboard);
     if (error_code != 0) {
         return error_code;
     }
 
     /* create reflector */
-    Reflector* reflector;
+    Reflector* reflector = nullptr;
     error_code = reflector->new_reflector(reflector_config_file, &reflector);
     if (error_code != 0) {
         delete plugboard;
@@ -53,7 +64,7 @@ int Enigma::create_enigma_machine(int argc, char** argv, Enigma** enigma) {
         }
 
         for (int i = 0; i < num_of_rotors; i++) {
-            Rotor* rotor;
+            Rotor* rotor = nullptr;
             error_code = rotor->new_rotor(rotor_files[i], rotor_starting_positions[i], &rotor);
             if (error_code != 0) {
                 delete plugboard;
@@ -76,8 +87,24 @@ vector<int> Enigma::get_rotor_starting_positions(string starting_pos_file) {
     ifstream input;
     input.open(starting_pos_file);
 
+    if (input.fail()) {
+        cerr << "Could not open rotor.pos file" << endl;
+        exit(ERROR_OPENING_CONFIGURATION_FILE);
+    }
+
     while(!input.eof()) {
-        input >> pos;
+        input >> pos >> ws;
+
+        if (input.fail()) {
+            cerr << "Non-numeric encountered in rotor.pos file" << endl;
+            exit(NON_NUMERIC_CHARACTER);
+        }
+
+        if (pos < 0 || pos > 25) {
+            cerr << "Out-of-range number encountered in rotor.pos file" << endl;
+            exit(INVALID_INDEX);
+        }
+
         starting_positions.push_back(pos);
     }
 
@@ -86,11 +113,67 @@ vector<int> Enigma::get_rotor_starting_positions(string starting_pos_file) {
     return starting_positions;
 }
 
-/* following main being used to test functions */
-int main(int argc, char** argv) {
-    Enigma* enigma;
+void Enigma::connect_rotors(vector<Rotor*> &rotors) {
+    int n = rotors.size();
 
-    int code = enigma->create_enigma_machine(argc, argv, &enigma);
+    if (n > 1) {
+        for (int i = 1; i < n; i++) {
+            rotors[i]->prev = rotors[i - 1];
+        }
+    } else if (n == 1) {
+        rotors[0]->prev = nullptr;
+    } else {
+        return;
+    }
+}
 
-    cout << code << endl;
+int Enigma::encrypt_input_message(string& message) {
+    string output_message;
+
+    for (int i = 0; i < message.length(); i++) {
+        if (message[i] < ASCII_A || message[i] > ASCII_Z) {
+            cerr << "Invalid character encountered. Please enter only upper-case letters A-Z" << endl;
+            return INVALID_INPUT_CHARACTER;
+        }
+        encrypt_character(message[i]);
+        output_message += message[i];
+    }
+    
+    cout << output_message << endl;
+
+    return NO_ERROR;
+}
+
+int Enigma::convert_char_to_int(char c) {
+    return (c - ASCII_A) % ALPHABET_SIZE;
+}
+
+char Enigma::convert_int_to_char(int n) {
+    return n + ASCII_A;
+}
+
+/* encrypt characters through each enigma component */
+void Enigma::encrypt_character(char &c) { 
+    int letter_as_num = convert_char_to_int(c);
+
+    letter_as_num = plugboard->map_letter(letter_as_num);
+
+    // rotate the right-most rotor
+    if (number_of_rotors > 0) {
+        created_rotors[number_of_rotors - 1]->rotate();
+    }
+
+    for (int i = number_of_rotors - 1; i >= 0; i--) { // map from right to left (last in array to first)
+        letter_as_num = (created_rotors[i])->map_forwards(letter_as_num);
+    }
+
+    letter_as_num = reflector->map(letter_as_num);
+
+    for (int i = 0; i < number_of_rotors; i++) { // map from left to right (first in array to last)
+        letter_as_num = (created_rotors[i])->map_backwards(letter_as_num);
+    }
+
+    letter_as_num = plugboard->map_letter(letter_as_num); // goes through plugboard again
+
+    c = convert_int_to_char(letter_as_num);
 }
--- enigma.h
+++ enigma.h
@@ -9,15 +9,21 @@
 
 class Enigma {
     private:
-        int number_of_rotors;
         Plugboard* plugboard;
         Reflector* reflector;
         vector<Rotor*> created_rotors;
+        int number_of_rotors;
 
     public:
         Enigma(Plugboard* plugboard, Reflector* reflector, vector<Rotor*> created_rotors, int number_of_rotors);
+        ~Enigma();
         int create_enigma_machine(int argc, char** argv, Enigma** enigma);
         vector<int> get_rotor_starting_positions(string starting_pos_file);
+        void connect_rotors(vector<Rotor*> &rotors);
+        int encrypt_input_message(string &message);
+        int convert_char_to_int(char c);
+        char convert_int_to_char(int n);
+        void encrypt_character(char &c);
 };
 
 #endif
