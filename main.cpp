f202de4cddd8b00f5828a91d52ed9ff0d41d11dc
Michael Lan <michael.lan20@imperial.ac.uk>
2020-11-20 14:15:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "enigma.h"
+#include "plugboard.h"
+#include "reflector.h"
+#include "rotors.h"
+#include "errors.h"
+#include "constants.h"
+
+int main(int argc, char** argv) {
+    Enigma* enigma = nullptr;
+    string message, message_wo_ws;
+
+    int code = enigma->create_enigma_machine(argc, argv, &enigma);
+    cout << "Enter message:" << endl;
+    getline(cin, message);
+
+    for (int i = 0; i < message.length(); i++) {
+        if (message[i] != ASCII_SPACE) {
+            message_wo_ws += message[i];
+        }
+    }
+
+    code = enigma->encrypt_input_message(message_wo_ws);
+}
